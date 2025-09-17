7f63d1c5afa4a2fef78e2d85da9b0d790cb21eee
Joshua-Tihabwangye <tihabwangyej@gmail.com>
2025-09-17 20:17:01 +0300

    Changes have been made in database

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /mnt/d/Projects/RMS256/.venv
+set -gx VIRTUAL_ENV /mnt/e/RMS256/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
